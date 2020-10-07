e727c6c536ff1808f7f33e3dad1de71d7e74ca38
lvendina <71191116+lvendina@users.noreply.github.com>
2020-10-07 09:34:41 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,143 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void write_dot(const char * fname, int n, int m,  int g[n][m])
+{
+    FILE * fo;
+    fo = fopen(fname,"wt");
+    fprintf(fo, "graph Graph {\n");
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            if (g[i][j] == 1)
+                fprintf( fo, "  %d -- %d;\n", i, j);
+    fprintf(fo, "}\n");
+    fclose(fo);
 
-struct tNode {
-    int data;
-    struct tNode *next;
-};
-typedef struct tNode tNode;
-
-struct tList {
-    int size;
-    tNode *head;
-};
-typedef struct tList tList;
-
-tList * create_list(void) {
-    tList *list = malloc(sizeof(tList));
-
-    list->size = 0;
-    list->head = NULL;
-
-    return list;
+    system("dot -Tpng g.dot -o g.png");
 }
 
-void push_front(tList *list, int value) {
-    tNode *Node = malloc(sizeof(tNode));
-    Node->data = value;
-    Node->next = list->head;
-
-    list->head = Node;
-    list->size += 1;
+void check_connect(int x, int u) {
+    if ((x-1)*(x-2)/2 < u)
+        printf("The graph IS connected");
+    else
+        printf("the graph is NOT connected");
+    return; 
 }
 
-void delete_front(tList *list) {
-    if (list->size == 0)
-        return;
+int main(void)
+{
+    printf("Enter size x*u of the matrix:\n");
+    int x, u;
+    while (scanf("%d", &x) != 1 || x < 0)
+        scanf("%*[^\n]");
+    while (scanf("%d", &u) != 1 || u < 0)
+        scanf("%*[^\n]");
+    int Graph[x][u];
 
-    tNode *t = list->head;
+    printf("Enter matrix:\n");
+    for (int i = 0; i < x; i++) 
+        for (int j = 0; j < u; j++) 
+            while (scanf("%d", &(Graph[i][j])) != 1 || (Graph[i][j] != 0 && Graph[i][j] != 1))
+                scanf("%*[^\n]");
 
-    list->head = list->head->next;
+    write_dot("g.dot", x, u, Graph);
 
-    free(t);
-    list->size -=1;
-}
-
-void insert(tList *list, int index, int value) {
-    if (index == 0)
-        push_front(list, value);
-    else {
-        tNode *pred = list->head;
-        for (int i = 0; i < index - 1; i++)
-            pred = pred->next;
-
-        tNode *Node = malloc(sizeof(tNode));
-        Node->data = value;
-        Node->next = pred->next;
-
-        pred->next = Node;
-        list->size += 1;
-    }
-}
-
-void deleteAt(tList *list, int index) {
-    if (index == 0)
-        delete_front(list);
-    else {
-        tNode *previos = list->head;
-        for (int i = 0; i < index - 1; i++)
-            previos = previos->next;
-
-        tNode *toDelete = previos->next;
-
-        previos->next = toDelete->next;
-
-        free(toDelete);
-        list->size -=1;
-    }
-}
-
-void print(tList *list) {
-
-    tNode *print_node = list->head;
-    while (print_node != NULL) {
-        printf("%d\n", print_node->data);
-        print_node = print_node->next;
-    }
-
-    Menu(list);
-}
-
-void add_node(tList *list) {
-    int i;
-    printf("Enter the item number from 1 to %d\n", list->size+1);
-    while (scanf("%d", &i) != 1 || i < 1 || i > list->size+1)
-        fflush(stdin);
-
-    int data;
-    printf("Enter data (number)\n");
-    while (scanf("%d", &data) != 1)
-        fflush(stdin);
-
-    insert(list, i-1, data);
-    printf("Item added!\n");
-
-    Menu(list);
-}
-
-void delete_node(tList *list) {
-
-    int i;
-    printf("Enter the item number from 1 to %d\n", list->size);
-    while (scanf("%d", &i) != 1 || i < 0 || i > list->size)
-        fflush(stdin);
-
-    deleteAt(list, i-1);
-    printf("Item deleted!\n");
-
-    Menu(list);
-}
-
-void Menu(tList *list) {
-    int c = 3;
-    printf("Click to add an item 1, for delete - 2, to display the list on the screen - 3\n");
-    while (scanf("%d", &c) != 1 || c < 1 || c > 3)
-        fflush(stdin);
-
-    if (c == 1)
-        add_node(list);
-    if (c == 2)
-        delete_node(list);
-    if (c == 3)
-        print(list);
-}
-
-int main(void) {
-
-    tList *list = create_list();
-    Menu(list);
+    int real_count_u = 0;
+    for (int i = 0; i < x; ++i)
+        for (int j = 0; j < u; ++j)
+            if (Graph[i][j] == 1)
+                ++real_count_u;
+    check_connect(x, real_count_u);
 
     return 0;
-}
-
+} 
